ea2114709f2cd9116dd6af24f2cb9f0fe19f6adb
Vivien Geoffroy <askformyemail@email.com>
2020-11-30 10:19:38 +0100

    adding fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,7 @@
+source ~/Code/wslenv/aliases.fish
+set fish_greeting
+set fish
+set -U fish_prompt_pwd_dir_length 0
+set  fish_color_user --bold green
+set  fish_color_host --bold green
+set  fish_color_cwd --bold blue
